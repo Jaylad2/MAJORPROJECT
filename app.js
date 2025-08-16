a281ae2b9633c9f5ca49cdd69e0009f800b8626a
Jaylad2 <Jaylad2305@gmail.com>
2025-08-16 21:50:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,9 +95,7 @@ app.use((req, res, next) => {
 //     let registeredUser = await User.register(fakeUser, "helloworld");
 //     res.send(registeredUser);
 // });
-app.get('/', (req, res) => {
-  res.redirect('/listings');
-});
+
 
 
 
